1390242a923469e363349d5f3e1c4e9cedacc07a
Ravi Mannanikkad <ravimannanikkad@gmail.com>
2020-03-16 19:40:06 +0100

    Created the main VHDL file BUTTON_LED.vhd

---
+++ Button_LED/Button_LED.srcs/sources_1/new/BUTTON_LED.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: Vaayoo Mantra Pvt. Ltf
+-- Engineer: Ravi Damodaran
+--
+-- Create Date: 03/16/2020 07:20:32 PM
+-- Design Name:
+-- Module Name: BUTTON_LED - Behavioral
+-- Project Name: BUTTON_LED
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity BUTTON_LED is
+--  Port ( );
+end BUTTON_LED;
+
+architecture Behavioral of BUTTON_LED is
+
+begin
+
+
+end Behavioral;
